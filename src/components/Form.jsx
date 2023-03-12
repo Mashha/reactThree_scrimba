eba7bc2e83bcbd5a749d91dd536c524b42722093
Masa Orel <orel.masha89@gmail.com>
2023-03-12 14:43:46 +0100

    added random image with state

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,23 +1,31 @@
 import memesData from "./memesData";
 
+import React from "react";
+
 export default function Form() {
-  function showUrl() {
-    const memesArray = memesData.data.memes
+  const [memeImage, setMemeImage] = React.useState("");
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
     const randomImage =
-      memesArray[
-        Math.floor(Math.random() * memesArray.length)
-      ];
-    console.log(randomImage.url);
+      memesArray[Math.floor(Math.random() * memesArray.length)];
+    setMemeImage(randomImage.url);
   }
   return (
-    <div className="form-container">
-      <div className="form">
-        <input type="text" placeholder="Top text" />
-        <input type="text" placeholder="Bottom text" />
-        <button className="submit-btn" onClick={showUrl}>
-          Get a new meme image 🖼
-        </button>
+    <>
+      <div className="form-container">
+        <div className="form">
+          <input type="text" placeholder="Top text" />
+          <input type="text" placeholder="Bottom text" />
+          <button className="submit-btn" onClick={getMemeImage}>
+            Get a new meme image 🖼
+          </button>
+        </div>
       </div>
-    </div>
+
+      <div className="image">
+        <img src={memeImage} alt="meme image" className="meme-image" />
+      </div>
+    </>
   );
 }
